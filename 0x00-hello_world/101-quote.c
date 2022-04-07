2e9bc4d2bae953ac700740d936b7687c91e800e8
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 11:22:14 -0700

    task 8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@ int main(void)
 
 	fwrite(str, sizeof(char), sizeof(str), stderr);
 
-	fwrite("\n", sizeof(char), 1, stderr);
+	fwrite("\n", sizeof(char), 0, stderr);
 
-	return (0);
+	return (1);
 }
