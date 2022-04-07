0d8a49a7fdf8fa4b8dbd06c988082a3c2d44138c
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 11:01:14 -0700

    final advanced task of the c - hello world project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fwrite(str, sizeof(char), sizeof(str), stderr);
+
+	fwrite("\n", sizeof(char), 1, stderr);
 
 	return (1);
 }
