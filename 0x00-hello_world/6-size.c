e0e421bd7ec630f9ad339afefdd01dcc65ab1193
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 06:03:40 -0700

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	printf("Size of an int: %zu bytes \n", sizeof(int));
 	printf("Size of a long int: %zu bytes \n", sizeof(long int));
 	printf("Size of a long long int: %zu bytes \n", sizeof(long long int));
+	printf("Size of a float: %zu bytes \n", sizeof(float));
 
 	return (0);
 }
