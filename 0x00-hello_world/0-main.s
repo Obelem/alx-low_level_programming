5a60ff8072019196658533a3d6d3adb4212ad2c5
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 08:50:11 -0700

    task 7 commit

--- 0x00-hello_world/0-main.s
+++ 0x00-hello_world/0-main.s
@@ -1,5 +1,9 @@
-	.file	"7-main.c"
+	.file	"0-main.c"
 	.intel_syntax noprefix
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Holberton School"
 	.text
 	.globl	main
 	.type	main, @function
@@ -12,6 +16,9 @@ main:
 	.cfi_offset 6, -16
 	mov	rbp, rsp
 	.cfi_def_cfa_register 6
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
+	call	printf@PLT
 	mov	eax, 0
 	pop	rbp
 	.cfi_def_cfa 7, 8
