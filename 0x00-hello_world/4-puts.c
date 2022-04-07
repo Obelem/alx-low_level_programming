9926742c5265a796b873312fc1ba7319e1077ebd
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 04:19:50 -0700

    demo puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
