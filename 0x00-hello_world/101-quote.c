ab9dbc9f7af6ba002205a8c2ab6e6f3fce964532
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 12:06:20 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	char str[59] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
 
 	fwrite(str, sizeof(char), sizeof(str), stderr);
 
