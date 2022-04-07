b13d37421f9b4e00a26f8f1578c4fa3386c08b1b
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-07 11:11:11 -0700

    task 8 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 
 	fwrite(str, sizeof(char), sizeof(str), stderr);
 
