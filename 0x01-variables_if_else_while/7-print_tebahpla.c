8708d8387fd2a12e74cf97220e9fb27acae5c5c0
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-08 07:07:38 -0700

    task 7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
