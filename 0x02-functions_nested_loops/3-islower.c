e105e0edd02dd50b5b5e0f5ceecc29775b434696
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 06:04:19 -0700

    islower_task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _is lower - checks for lowercase character
+ *
+ * Return: 1 if c is lowercase or 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		
+		return (1);
+	else
+		return (0);
+}
