542ed369a44364ae4cd18de010d1d7639d87af21
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 07:35:49 -0700

    task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: Always 0 (Success)
+ * @int: The number to be worked on
+ */
+int _abs(int)
+{
+	if (int < 0)
+		int = int * -1;
+		_putchar(int);
+
+	else
+		_putchar(int);
+}
