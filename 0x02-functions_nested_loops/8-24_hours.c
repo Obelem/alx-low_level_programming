e1268eb7405f85edb2c33573ca4f222824ce9a5d
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 10:11:19 -0700

    task 8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j < 4; j++)
 		{
@@ -18,11 +18,11 @@ void jack_bauer(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
 			}
