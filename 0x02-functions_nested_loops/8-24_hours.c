7937e26f36424cf215f847eee1b9ccbd31ea88d0
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 09:47:24 -0700

    task 8 commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of Jack's day
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
