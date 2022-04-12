14c3a315005c86917931df41d3f4240e1cacce44
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 07:07:55 -0700

    task t commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 }
