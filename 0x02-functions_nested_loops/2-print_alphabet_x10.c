df398c0ca53e5830aa19b1f523a1157ebe76780a
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 05:49:48 -0700

    print alphabet task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+voind print_alphabet_x10(void)
+{
+	int alphabet;
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
