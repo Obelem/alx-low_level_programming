9ecffdd90f284d4bde57df531343fbdebf729f4f
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 06:07:46 -0700

    task 3 commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _is lower - checks for lowercase character
- *
+ *@c: contains value to be compared
  * Return: 1 if c is lowercase or 0 if otherwise
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		
+
 		return (1);
 	else
 		return (0);
