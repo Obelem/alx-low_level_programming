740b0909d5bf75bfa0fcd6bc81667b060402d683
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 15:14:32 -0700

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds tow integers and returns the result
+ *
+ * @a: first integer to be added
+ * @b: second integer to be added
+ * Return: the result of adding two integers
+ */
+int add(int a, int b)
+{
+	int ans = a + b;
+
+	return (ans);
+}
