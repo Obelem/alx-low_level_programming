1c8df385d464790d5d6d1635f8a9f5d523531951
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-12 09:02:52 -0700

    last digit commit_task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: number to print last digit
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+
+	return (n);
+}
