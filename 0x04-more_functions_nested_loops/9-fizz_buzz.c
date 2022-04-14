bc4b5afb68810cb7484cbf38a22078d7ad39edaa
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-14 16:47:46 -0700

    task 9 commit: fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d ", i);
+
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+
+		if (i % 3 != 0 && i % 5 == 0)
+			printf("Buzz ");
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+	}
+	return (0);
+}
