0adaeb85d0a19794e81d688bf9230f543615a94f
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-14 15:58:54 -0700

    task 8 commit: squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a squre followed by a new line
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= size; col++)
+			{
+				_putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
