9cd7249d071b40ef1487b04df88e7cedb365c6d7
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-14 09:22:15 -0700

    task 3 commit: The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
