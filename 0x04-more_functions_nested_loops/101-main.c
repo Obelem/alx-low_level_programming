8b2792a503e07d4e3502dfab4b0aa5bd5b2aa6ad
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-14 17:54:52 -0700

    task 12 commit: final advanced task

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -11,5 +11,11 @@ int main(void)
 	_putchar('\n');
 	print_number(402);
 	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+       	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
 	return (0);
 }
