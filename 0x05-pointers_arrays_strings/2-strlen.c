84abf13cfbe83ad07f13bd04de1238e3a56d8904
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 05:06:46 -0700

    task 3: I don not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to be counted
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*p != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
