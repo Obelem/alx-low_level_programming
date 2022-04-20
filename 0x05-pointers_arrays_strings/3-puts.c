73b119a86b23d051cc7f5429c57031a8d66438b6
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 06:25:31 -0700

    task 3: I don not fear computers....commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ *
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
