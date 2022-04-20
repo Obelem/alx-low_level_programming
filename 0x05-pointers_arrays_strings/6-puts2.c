fccbad5841deb0a3b3d057fa5df08a0732736ff1
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 13:42:46 -0700

    task 5: A good engineer thinks in reverse......commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with first character, followed by /n
+ *
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int len = _strlen(str);
+
+	printf("%d \n", len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
