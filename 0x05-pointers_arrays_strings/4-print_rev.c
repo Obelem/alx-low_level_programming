1682e6cb50cca47ab4d565477a22a4d8e651abcc
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 08:36:23 -0700

    task 4: I can only go one way...commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ *
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int l;
+
+	l = 0;
+	
+	while (*(s + l) != '\0')
+		l++;
+	l--;
+	for (; l >= 0; l--)
+		_putchar(*(s + l));
+	_putchar('\n');
+}
