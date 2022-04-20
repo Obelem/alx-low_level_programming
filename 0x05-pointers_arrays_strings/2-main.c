e524cc36213809d71dec294308b79b42fffabe60
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 05:12:54 -0700

    task 2: This report,........being read commit

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,7 +11,7 @@ int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*p != '\0')
+	while (*s != '\0')
 	{
 		count++;
 		s++;
