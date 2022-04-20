195aca8e5fe8ec5e8b931e53a57a141bededa899
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 15:57:45 -0700

    task 11: Don't hate the hacker, hate the code commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,9 +16,8 @@ int main(void)
 	{
 		c = rand() % 128;
 		sum += c;
-		putchar(c);
-		putchar('\0');
+		printf("%c\0", c);
 	}
-	putchar(2772 - sum);
+	printf("%c\0", 2772 - sum);
 	return (0);
 }
