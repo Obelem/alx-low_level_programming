e5e0044c5af736044e213f854eb3f87392eabea2
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-20 03:14:53 -0700

    task 0_98 Battery st. commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter and
+ * updates the value it points to 98
+ *
+ * @n: pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# 0x05 C - Pointers, arrays and strings
+This project contains all the tasks for the project stated above.
