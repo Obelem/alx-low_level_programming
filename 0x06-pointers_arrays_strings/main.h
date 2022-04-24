8dcf6684c7699d8170c7d558dbe177596925b002
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-24 13:10:06 -0700

    task 6: capitalize all words of a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
