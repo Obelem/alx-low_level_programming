a880dba2c611521c78fe970cf9176afc0b048c0c
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-26 06:31:03 -0700

    task 2: locates character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s+i);
 	}
-	return (NULL);
+	return ('\0');
 }
