93a8f1cd8c4f4bb740b0de3c881f2026dd66b85c
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-26 04:33:16 -0700

    task 0_ A custom memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: ptr to starting address of desired memory block
+ * @b: value to fill
+ * @n: number of bytes to set
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
