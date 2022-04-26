9a276e0d1b8c317c6ca32df3949018f7683ba789
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-26 15:04:39 -0700

    task 2 commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (s+i);
+		if (*s == c)
+			return (s);
+		++s;
 	}
-	return ('\0');
+	if (*s == c)
+		return (s);
+	return (0);
 }
