20900e6f14e9e1d71ca2910d9627b6a60f824b08
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-26 04:34:04 -0700

    task 0_ A custom memset function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
