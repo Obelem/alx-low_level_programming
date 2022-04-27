89552a6de7c361491a00898545600dbae8bea1b9
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-27 11:46:47 -0700

    task 7: prints sum of the 2 diagonals of a square matrix of integers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
 #endif
