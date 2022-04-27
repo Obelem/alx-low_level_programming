f3f4ea667e04bd52d4d223979bd87d4d288f1aea
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-27 14:17:04 -0700

    sets value of a pointer to a char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif
