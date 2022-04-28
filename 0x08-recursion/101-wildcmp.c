9bf5378facf682246523297462eecc9b14fef64b
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-28 16:32:06 -0700

    task 8: compares two strings to check for identicalness

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings to find if they're identical
+ * @s1: string 1
+ * @s2: string 2
+ * Return: return 1 or 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+
+		if (wildcmp(s1 + 1, s2) || wildcpm(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
