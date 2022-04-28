bf3f17926f2c42f66d0584f8b883f4540af03602
Braide Jesse <ibbraidejesse@gmail.com>
2022-04-28 15:09:28 -0700

    task 5: recursive square root

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_calc(int n, int x);
 
 
 #endif /* MAIN_H */
