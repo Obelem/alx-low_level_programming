dd3fba672d776bf2df5b2b5746b6088f00af3649
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-04 12:04:03 -0700

    task 4: adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argc[])
+{
+	int i, j, add =m 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+	else if (argc == 1)
+		printf("0\n");
+
+	return (0);
+}
