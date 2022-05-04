e14f5d034ef7b9f03211a9408a8cb9138cd13584
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-04 11:26:14 -0700

    task 0: prints name of program, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# 0x0A. C -argc, argv
+This contains all the tasks for the above mentioned project
