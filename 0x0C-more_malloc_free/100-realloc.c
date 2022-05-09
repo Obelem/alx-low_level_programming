3d908735aa83d8f778bb7c629b850ba26ca49a8b
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-09 14:02:06 -0700

    task 4: creating my custom realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,6 +28,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 		new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
 
 	/* assign ptr to clone_ptr to use pointer arithmetic */
 	/* N/B: Did this bcos void pointers dont do arithmetic */
