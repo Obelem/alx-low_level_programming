0b7b3625f5022e6bb09cc6a5eed0594c57741aca
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-09 06:19:33 -0700

    task 1: concatenates two strings

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdio.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+#endif /* happy coding bro */
