3aedae4316ec1eab6a07c1916fb7f78ab11adf37
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-10 06:30:15 -0700

    task 5: multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * main - multiplies tow positive numbers
@@ -9,11 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul, i, j;
+	int num1, num2, mul, i, j, k;
+	char err[] = "error";
+	char *ans;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		for (k = 0; err[k] != '\0'; k++)
+			_putchar(err[k]);
+
+		_putchar('\n');
 		exit(98);
 	}
 	for (i = 1; i < argc; i++)
@@ -22,12 +26,71 @@ int main(int argc, char *argv[])
 		{
 			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
-				printf("Error\n");
+				for (k = 0; err[k] != '\0'; k++)
+					_putchar(err[k]);
+
+				_putchar('\n');
 				exit(98);
 			}
 		}
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	mul = num1 * num2;
+
+	ans = _itoa(mul, 1);
+	for (i = 0; ans[i] != '\0'; i++)
+		_putchar(ans[i]);
+
+	_putchar('\n');
 	return (0);
+}
+/**
+ * _atoi - convert ascii to integer
+ * @s: string to convert
+ * Return: corresponding integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int num = 0;
+
+	while (s[i] != 0)
+	{
+		num = (num * 10) + (s[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * _itoa - integer to ascii
+ * @n: integer to convert
+ * @i: divisor
+ * Return: pointer to string
+ */
+char *_itoa(int n, int i)
+{
+	int count = 0;
+	int j = 0;
+	char *ptr;
+
+	while (i <= n)
+	{
+		i = i * 10;
+		count++;
+	}
+	ptr = malloc((count + 1) * sizeof(char));
+	i = i / 10;
+
+	while (j < count)
+	{
+		ptr[j] = (n / i) + '0';
+		n = n % i;
+		i = i / 10;
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(char *s);
+char *_itoa(int n, int i);
 
 #endif /* happy coding bro */
