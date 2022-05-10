6a90818690357df3b39cc4dc8d92d6d7e13136db
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-10 09:29:07 -0700

    task 5: multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,51 +1,6 @@
 #include "main.h"
 
 /**
- * main - multiplies tow positive numbers
- * @argc: arg count
- * @argv: arg vector
- * Return: 0, Always Success
- */
-int main(int argc, char *argv[])
-{
-	int num1, num2, mul, i, j, k;
-	char err[] = "error";
-	char *ans;
-
-	if (argc != 3)
-	{
-		for (k = 0; err[k] != '\0'; k++)
-			_putchar(err[k]);
-
-		_putchar('\n');
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if ((argv[i][j] < 48 || argv[i][j] > 57) && argv[i][j] != '-')
-			{
-				for (k = 0; err[k] != '\0'; k++)
-					_putchar(err[k]);
-
-				_putchar('\n');
-				exit(98);
-			}
-		}
-	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	mul = num1 * num2;
-
-	ans = _itoa(mul, 1);
-	for (i = 0; ans[i] != '\0'; i++)
-		_putchar(ans[i]);
-
-	_putchar('\n');
-	return (0);
-}
-/**
  * _atoi - convert ascii to integer
  * @s: string to convert
  * Return: corresponding integer
@@ -118,4 +73,49 @@ char *_itoa(int n, int i)
 	}
 	ptr[j] = '\0';
 	return (ptr);
+}
+/**
+ * main - multiplies tow positive numbers
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0, Always Success
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul, i, j, k;
+	char err[] = "error";
+	char *ans;
+
+	if (argc != 3)
+	{
+		for (k = 0; err[k] != '\0'; k++)
+			_putchar(err[k]);
+
+		_putchar('\n');
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if ((argv[i][j] < 48 || argv[i][j] > 57) && argv[i][j] != '-')
+			{
+				for (k = 0; err[k] != '\0'; k++)
+					_putchar(err[k]);
+
+				_putchar('\n');
+				exit(98);
+			}
+		}
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	mul = num1 * num2;
+
+	ans = _itoa(mul, 1);
+	for (i = 0; ans[i] != '\0'; i++)
+		_putchar(ans[i]);
+
+	_putchar('\n');
+	return (0);
 }
