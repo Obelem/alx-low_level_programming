787803d38d1e8cd29c4a7b27aff1a68a090dbb77
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-11 07:49:30 -0700

    task 0: defining my first struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# 0x0E C- Sttructures, typedef
+This directory contains all the tasks for the above mentioned project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
