4737d0f5928faafd2734d9dcbb550c3f3aa45128
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-11 14:40:12 -0700

    task 5: frees dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog struct
+ * @d: pointer to dog struct to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return (NULL);
+
+	free(d.name);
+	free(d.owner);
+	free(d);
+}
