0dbfb2d394f39d5801bd2a9331f1674a293195d7
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-11 14:45:31 -0700

    task 5: frees dog struct

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,10 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return (NULL);
-
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
