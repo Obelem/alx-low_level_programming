235067d73d3c77fc7940b6f539cb74de553467d8
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-11 14:41:22 -0700

    task 5: frees dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,6 @@ int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
