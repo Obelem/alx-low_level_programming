ad967ed2f7c8da80a77e76a5b3fb7c9111bf37c6
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-13 06:48:23 -0700

    task 1: uses a call back to print each element of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a callback on each array element
+ * @array: array to iterate over
+ * @size: size of array
+ * @action: call back function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == 0 || action == 0)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
