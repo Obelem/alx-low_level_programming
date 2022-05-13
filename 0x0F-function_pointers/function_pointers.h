1111b8bc6757dc0b902f082d3744c9ab23d6273d
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-13 06:31:19 -0700

    task 0: prints name using a function pointer as callback

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
