32940246a445835615aa692f99c1121688ef2105
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-13 06:25:21 -0700

    task 0: prints name using a function pointer as callback

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name using function pointer as callback
+ * @name: name to print
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
