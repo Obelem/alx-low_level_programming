37870b45203ab3c9cbad9ca1f6c33cbc812ccc31
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-16 15:06:01 -0700

    task 3: prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,12 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/* Added functions to print_all source code */
+void print_c(va_list c);
+void print_s(va_list s);
+void print_i(va_list i);
+void print_f(va_list f);
+
 
 
 #endif
