5de98e58f9c9727fe85d67e73855622f5b2b4e46
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-16 10:21:54 -0700

    task 1: prints variable int args followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL)
-		return;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -23,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		res = va_arg(args, int);
 		printf("%d", res);
 
-		if (i != n - 1)
+		if (i != n - 1 && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
