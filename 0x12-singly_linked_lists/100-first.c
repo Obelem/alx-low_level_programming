2483c3656e09fb6766eca7c4cfc889bc32b19368
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-24 17:12:24 -0700

    task 5: prints stuff before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_before_main(void) __attribute__ ((constructor));
+
+/**
+ * print_before_main - prints stuff before main
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
