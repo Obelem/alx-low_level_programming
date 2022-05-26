d13a32ed08ec99eab06637ffb370135da76c9a46
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-26 13:34:04 -0700

    task 9: inserts node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *temp = *head;
-	listint_int *new;
+	listint_t *new;
 
 	/* initialize new node */
 	new = malloc(sizeof(listint_t));
