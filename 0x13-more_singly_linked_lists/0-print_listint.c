9fab71f849d8f2bb0cef776294c1b359db3bce8a
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-26 05:45:25 -0700

    task 0: prints all elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list_t list
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	long unsigned int count = 0;
+
+	while (h)
+	{
+		printf("%lu\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list
+{
+	unsigned long int n;
+	struct list *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
