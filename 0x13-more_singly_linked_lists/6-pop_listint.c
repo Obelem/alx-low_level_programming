61b23fe1fd9a270e73de44ea17f9b4ebaa986eb9
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-26 11:05:09 -0700

    task 6: delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: double pointer to head node
+ * Return: head's node data or 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	/* Save value of current head node before deletion */
+	n = (*head)->n;
+
+	/* delete head node */
+	*head = (*head)->next;
+
+	return (n);
+}
