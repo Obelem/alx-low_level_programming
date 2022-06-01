550c967c65be2809118c509842d5c41c926a8e71
Braide Jesse <ibbraidejesse@gmail.com>
2022-06-01 12:52:28 -0700

    task 1: creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: content of created file
+ * Return: 1 for success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, num_written;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (len = 0; text_content[len] != '\0'; )
+		len++;
+
+	num_written = write(fd, text_content, len);
+
+	if (!num_written)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
