67ee1110721e104ed027cd7071947685248df024
Braide Jesse <ibbraidejesse@gmail.com>
2022-06-01 13:54:47 -0700

    task 2: appends text to end of file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to end of a file
+ * @filename: name of file
+ * @text_content: text to be appended
+ * Return: 1 for success, 0 for otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, num_written, len;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, 0_WRONLY | 0_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; )
+			len++;
+
+		num_written = write(fd, text_content, len);
+
+		if (num_written == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
