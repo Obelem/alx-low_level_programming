887e86f6d1b0d9ed5aa495410987e727aa221bdf
Obelem <ibbraidejesse@gmail.com>
2022-07-29 23:18:20 +0100

    code test

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL && !node)
+			continue;
+
+		if (!node)
+			node = ht->array[i];
+		printf("'%s': '%s'", node->key, node->value);
+
+		node = node->next;
+		while (!node && i < ht->size)
+			node = ht->array[++i];
+		if (node)
+			printf(", ");
+	}
+	printf("}\n");
+}
