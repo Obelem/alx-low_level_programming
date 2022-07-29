813f0778356b78a14989ebcad846aa224d213aea
Obelem <ibbraidejesse@gmail.com>
2022-07-29 20:24:04 +0100

    freed hashtable and key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -19,5 +19,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		if (strcmp(ht->array[i]->key, key) == 0)
 			return (ht->array[i]->value);
 	}
+	free(ht);
+	free(key);
 	return (NULL);
 }
