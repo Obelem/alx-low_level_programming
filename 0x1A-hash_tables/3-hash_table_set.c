8e17a8c9b3ae68c3bb994824f887c9aaba13ba41
Obelem <ibbraidejesse@gmail.com>
2022-07-29 19:35:34 +0100

    troubleshooting

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,51 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to hash table
- * @ht: hash map
- * @key: element key
- * @value: element value
- * Return: 1 on success, 0 otherwise
+ * hash_table_set - Adds or update an element to the hash table.
+ * @ht: hash table.
+ * @key: key of element
+ * @value: value of element
+ * Return: 1 on success or 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx;
-	hash_node_t *currNode, *newNode;
-	char *valDup;
+	hash_node_t *new;
+	unsigned long int index, i;
+	char *value_copy;
 
-	if (*key == '\0' || !ht || !value || !key)
+	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
-	valDup = malloc(sizeof(char) * strlen(value));
-	if (!valDup)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
-	valDup = strdup(value);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	for (currNode = ht->array[idx]; currNode; currNode = currNode->next)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(value, currNode->value) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			currNode->value = valDup;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
-	newNode = malloc(sizeof(hash_node_t));
-	if (!newNode)
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 	{
-		free(valDup);
+		free(value_copy);
 		return (0);
 	}
-	newNode->key = strdup(key);
-	if (newNode->key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(newNode);
-		return (NULL);
+		free(new);
+		return (0);
 	}
-	newNode->value = valDup;
-	newNode->next = ht->array[idx];
-	ht->array[idx] = newNode;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
