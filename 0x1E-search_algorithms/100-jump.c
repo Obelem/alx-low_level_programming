3e974cf5b5617fbf8b0e0a3622c2255bb5ef995c
Jesse <ibbraidejesse@gmail.com>
2022-11-02 13:38:08 +0100

    added null check

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,6 +11,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t start = 0, i, end;
 
+	if (!array || size == 0)
+		return (-1);
+
 	end = sqrt(size);
 
 	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
