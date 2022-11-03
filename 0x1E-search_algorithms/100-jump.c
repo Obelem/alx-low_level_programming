f5a6700025034d69d5a526a9043e66f5e4663c10
Jesse <ibbraidejesse@gmail.com>
2022-11-03 05:23:49 +0100

    implemented jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,40 +1,42 @@
 #include "search_algos.h"
 /**
- * jump_search - searches for a value in a sorted array of integers
- * using the Jump search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * Return: first index where value is located or -1 otherwise
-*/
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t start = 0, i, end;
+	int index, m, k, prev;
 
-	if (!array || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	end = sqrt(size);
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
-	while (array[end] < value && start < size)
-	{
-		printf("Value checked array[%ld] = [%d]\n", end, array[end]);
-		start = end;
-		end = end + sqrt(size);
-		if (end > size - 1)
-			break;
-	}
-	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
-	i = start;
-	while (i <= end && end <= size - 1)
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
 	return (-1);
 }
