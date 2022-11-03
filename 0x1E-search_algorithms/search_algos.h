b8417972a545dd8a7cbbc33449af839a176530f6
Jesse <ibbraidejesse@gmail.com>
2022-11-03 05:31:32 +0100

    implemented interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_recursion(int *array, int value, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
