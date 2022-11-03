61f33ff87b25aa20840d5aba01d3162d81557a46
Jesse <ibbraidejesse@gmail.com>
2022-11-03 05:47:54 +0100

    implemented advanced bin search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int binary_recursion(int *array, int value, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
